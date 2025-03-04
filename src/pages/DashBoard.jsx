72833c18d920318b44844926cb11e06fda989b5e
keshav <k21keshav05@gmail.com>
2025-03-05 03:54:11 +0530

    Handle Logout

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -50,7 +50,11 @@ export default function Dashboard() {
         {/* Navbar */}
         <nav className="bg-white shadow-lg py-4 px-6 flex justify-between items-center">
           <h2 className="text-xl font-semibold text-blue-600">Dashboard</h2>
-          <button className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600">Logout</button>
+          <Link to="/">
+            <button className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600">
+              Logout
+            </button>
+          </Link>
         </nav>
 
         {/* Page Content */}
